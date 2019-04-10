5bbd0e5ba009016b4a10091d116c717474c6990e
WebFreak001 <gh@webfreak.org>
2019-04-10 21:04:57 +0200

    push built spritesheet map to git

--- pack-pngs.sh
+++ pack-pngs.sh
@@ -2,4 +2,7 @@
 
 # Requires crunch (https://github.com/WebFreak001/crunch), installed as "crunch-pack"
 
-crunch-pack res/spritesheet res/sprites -p2 -c -t -u -ba64 -v
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+crunch-pack res/spritesheet res/sprites -p2 -c -t -u -ba64 -v || printf "${RED}crunch-pack failed! Did you install it from https://github.com/WebFreak001/crunch or is it crashing?${NC} Textures will not be updated!"
