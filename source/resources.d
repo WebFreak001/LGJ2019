8c9bb71528438c8103a3c497d8b2e1c4bc442352
WebFreak001 <gh@webfreak.org>
2019-04-11 18:46:51 +0200

    added white pixel textures

--- source/resources.d
+++ source/resources.d
@@ -6,7 +6,7 @@ import std.exception;
 
 struct Resources
 {
-	alias usedSprites = AliasSeq!("player");
+	alias usedSprites = AliasSeq!("player", "white", "white2x", "white4x");
 
 	Spritesheet spritesheet;
 	// magically wraps all the used sprites as easy to access variables
