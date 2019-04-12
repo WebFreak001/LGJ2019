883681abc16dc9bc6be9f4330704e69642854727
WebFreak001 <gh@webfreak.org>
2019-04-12 18:04:10 +0200

    removed 4 color background

--- source/game/systems.d
+++ source/game/systems.d
@@ -7,6 +7,7 @@ import resources;
 import game.components;
 import game.world;
 
+import std.algorithm;
 import std.meta;
 import std.stdio;
 
@@ -150,14 +151,6 @@ struct DrawSystem
 	void draw(ref GameWorld world, IRenderTarget window)
 	{
 		spriteBatch.begin(R.spritesheet.textures[0]);
-
-		spriteBatch.drawSprite(R.sprites.white4x, vec2(0, 0), vec2(100, 304) / 4.0f, vec4(1, 0, 0, 1));
-		spriteBatch.drawSprite(R.sprites.white4x, vec2(100, 0), vec2(100,
-				304) / 4.0f, vec4(0, 1, 0, 1));
-		spriteBatch.drawSprite(R.sprites.white4x, vec2(200, 0), vec2(100,
-				304) / 4.0f, vec4(0, 0, 1, 1));
-		spriteBatch.drawSprite(R.sprites.white4x, vec2(300, 0), vec2(100,
-				304) / 4.0f, vec4(1, 1, 0, 1));
 
 		foreach (ref entity; world.entities)
 		{
