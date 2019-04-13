0fd7effba70ef9da791882a16ff9769e6b1f85ea
Reavershark <jonasty1999@gmail.com>
2019-04-13 19:41:19 +0200

    Transparent tiles

--- pack-pngs.sh
+++ pack-pngs.sh
@@ -5,4 +5,4 @@
 RED='\033[0;31m'
 NC='\033[0m' # No Color
 
-crunch-pack res/spritesheet res/sprites -p2 -c -t -u -ba64 -v || printf "${RED}crunch-pack failed! Did you install it from https://github.com/WebFreak001/crunch or is it crashing?${NC} Textures will not be updated!"
+crunch-pack res/spritesheet res/sprites -p2 -c -u -ba64 -v || printf "${RED}crunch-pack failed! Did you install it from https://github.com/WebFreak001/crunch or is it crashing?${NC} Textures will not be updated!"
